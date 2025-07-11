6a5a0d56251270065911cf7f6e660338a3799b59
Corey McCormick <coreymccor@live.com.au>
2025-07-11 09:57:52 +0000

    Improve post create command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,6 @@ echo "max_input_vars = 6000
 upload_max_filesize = 50M
 post_max_size = 51M
 " | sudo tee /usr/local/etc/php/conf.d/gibbon.ini
-chown -R www-data:www-data $(pwd) && chmod -R 755 $(pwd) && chmod -R 774 $(pwd)/uploads
+# chown -R www-data:www-data "$(pwd)/!(.git)" && chmod -R 755 $(pwd) &&
+chmod -R 774 $(pwd)/uploads
 # sudo apache2ctl start
