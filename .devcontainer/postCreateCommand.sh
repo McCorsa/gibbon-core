e1f263670a5eb766337d1669880b052ab4d30240
Corey McCormick <57275965+McCorsa@users.noreply.github.com>
2025-07-12 08:35:44 +0000

    Increase max input vars

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,7 @@ sudo chmod a+x "$(pwd)"
 sudo rm -rf /var/www/html
 sudo ln -s "$(pwd)" /var/www/html
 sudo a2enmod rewrite
-echo "max_input_vars = 6000
+echo "max_input_vars = 8000
 upload_max_filesize = 50M
 post_max_size = 51M
 " | sudo tee /usr/local/etc/php/conf.d/gibbon.ini
